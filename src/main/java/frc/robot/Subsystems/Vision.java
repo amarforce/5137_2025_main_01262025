0a3e4fb8e42d0d81643ff371fe9a88c810f81da8
JosephSwearingen <jhsweargn@gmail.com>
2025-01-17 09:25:39 -0800

    Added object detection camera

--- src/main/java/frc/robot/Subsystems/Vision.java
+++ src/main/java/frc/robot/Subsystems/Vision.java
@@ -6,6 +6,7 @@ import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -21,6 +22,8 @@ public class Vision extends SubsystemBase {
     private PhotonCamera leftCamera;
     private PhotonPoseEstimator leftPoseEstimator;
 
+    private PhotonCamera frontObjectCamera;
+
     public Vision() {
         fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
@@ -29,6 +32,8 @@ public class Vision extends SubsystemBase {
 
         leftCamera = new PhotonCamera("leftCamera");
         leftPoseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Vision_Constants.robotToLeftCamera);
+
+        frontObjectCamera = new PhotonCamera("frontObjectCamera");
     }
 
     //TODO: Switch to new method when docs update
@@ -39,5 +44,9 @@ public class Vision extends SubsystemBase {
 
     public Optional<EstimatedRobotPose> getLeftPose() {
         return leftPoseEstimator.update(leftCamera.getLatestResult());
+    }
+
+    public PhotonTrackedTarget getClosestCoral() {
+        return frontObjectCamera.getLatestResult().getBestTarget();
     }
 }
