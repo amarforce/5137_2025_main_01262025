4b28dfe9f559d7f81b3e1b093e9658bf9eebe507
JosephSwearingen <jhsweargn@gmail.com>
2025-01-16 19:17:03 -0800

    Added outline for pose estimation method with single camera, removed periodic method

--- src/main/java/frc/robot/Constants/Vision_Constants.java
+++ src/main/java/frc/robot/Constants/Vision_Constants.java
@@ -1,5 +1,9 @@
 package frc.robot.Constants;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 public class Vision_Constants {
-    
+    public static final Transform3d robotToFrontCamera = new Transform3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0));
 }
--- src/main/java/frc/robot/Subsystems/Vision.java
+++ src/main/java/frc/robot/Subsystems/Vision.java
@@ -1,26 +1,31 @@
 package frc.robot.Subsystems;
 
-import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonTrackedTarget;
+import java.util.Optional;
 
-import edu.wpi.first.math.geometry.Transform3d;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Vision_Constants;
 
 public class Vision extends SubsystemBase {
+    private AprilTagFieldLayout fieldLayout;
+
     private PhotonCamera frontCamera;
+    private PhotonPoseEstimator frontPoseEstimator;
 
     public Vision() {
+        fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
         frontCamera = new PhotonCamera("frontCamera");
+        frontPoseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Vision_Constants.robotToFrontCamera);
     }
 
-    @Override
-    public void periodic() {
-        var result = frontCamera.getLatestResult();
-        if (result.hasTargets()) {
-            PhotonTrackedTarget target = result.getBestTarget();
-            int id = target.getFiducialId();
-            Transform3d pose = target.getBestCameraToTarget();
-            System.out.println("Target: " + id + "Pose: " + pose);
-        }
+    public Optional<EstimatedRobotPose> getFrontPose() {
+        return frontPoseEstimator.update(frontCamera.getLatestResult());
     }
 }
