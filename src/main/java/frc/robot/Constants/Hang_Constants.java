77bddfabcdadd32989a74d46a7c235d197db9c91
quesadilla2147 <165604228+quesadilla2147@users.noreply.github.com>
2025-01-17 23:46:13 -0800

    working on subsystem

--- src/main/java/frc/robot/Constants/Hang_Constants.java
+++ src/main/java/frc/robot/Constants/Hang_Constants.java
@@ -2,15 +2,10 @@ package frc.robot.Constants;
 
 public class Hang_Constants {
     public static final class Pneumatics {
-        // Port for the REV Pneumatics Hub compressor (assume default port 0)
-        public static final int COMPRESSOR_PORT = 0;
-
-        // Channels for the solenoids connected to the REV Pneumatics Hub
-        public static final int SOLENOID_1_CHANNEL = 1;
-        public static final int SOLENOID_2_CHANNEL = 2;
-
-        // Pressure thresholds for the compressor's analog control
-        public static final int MIN_PRESSURE = 0; // Start compressor below this PSI
-        public static final int MAX_PRESSURE = 120; // Stop compressor above this PSI
+        public static final int COMPRESSOR_PORT = 0; // Port for the compressor on the Pneumatics Hub
+        public static final int SOLENOID_1_CHANNEL = 1; // Channel for Solenoid 1
+        public static final int SOLENOID_2_CHANNEL = 2; // Channel for Solenoid 2
+        public static final int MIN_PRESSURE = 0; // Min pressure threshold to turn the compressor on
+        public static final int MAX_PRESSURE = 120; // Max pressure threshold to turn the compressor off
     }
 }
--- src/main/java/frc/robot/Subsystems/Hang_Subsystem.java
+++ src/main/java/frc/robot/Subsystems/Hang_Subsystem.java
@@ -1,10 +1,40 @@
 package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Hang_Constants.Pneumatics;
+import frc.robot.Constants.Hang_Constants;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.hal.REVPHJNI;
 import edu.wpi.first.wpilibj.Compressor;
 
 
-public class Hang_Subsystem {
+
+
+public class Hang_Subsystem extends SubsystemBase {
+    
+    
+    
+    
+    
+    public void PneumaticsSubsystem() {
+        final Solenoid o_solenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
+        final Solenoid t_solenoid = new Solenoid(PneumaticsModuleType.REVPH, 1);
+
+        
+    }
+
+    public double initializeCompressor() {
+        final Compressor compressor = new Compressor(PneumaticsModuleType.REVPH);
+        compressor.enableAnalog(70, 120);
+        return compressor.getPressure();
+
+    }
+       
+         
+
+    
+
+        
     
 }
