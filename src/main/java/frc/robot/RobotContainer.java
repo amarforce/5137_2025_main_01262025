09a7a355c323c44136a136075ef44bd6c49a31b8
npasetto0 <blasphemorgulus@gmail.com>
2025-01-18 15:15:17 -0800

    Copied code over

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,25 +1,54 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PS5Controller;
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Subsystems.*;
+import frc.robot.subsystems.Elevator;
+import frc.robot.commands.ElevatorCommands;
+import frc.robot.elastic.Reef;
 
 public class RobotContainer {
-
-  private Vision vision;
+  private Elevator elevator;
+  private ElevatorCommands elevatorCommands;
+  private PS5Controller driver = new PS5Controller(0);
+  private PS5Controller operator = new PS5Controller(1);
+  private Reef reef=new Reef();
 
   public RobotContainer() {
-    
-    vision = new Vision();
-
+    elevator = new Elevator();
+    elevatorCommands = new ElevatorCommands(elevator);
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    //elevator.setManualControl(true);
+    elevator.setDefaultCommand(elevatorCommands.setGoal(()->1-operator.getLeftY()));
+    EventLoop event=new EventLoop();
+    event.bind(()->elevatorCommands.moveToL4());
+    operator.triangle(event);
+
+    EventLoop event2=new EventLoop();
+    event2.bind(()->elevatorCommands.moveToL3());
+    operator.circle(event2);
+
+    EventLoop event3=new EventLoop();
+    event3.bind(()->elevatorCommands.moveToL2());
+    operator.square(event3);
+
+    EventLoop event4=new EventLoop();
+    event4.bind(()->elevatorCommands.moveToL1());
+    operator.cross(event4);
+  }
+
+  private void telemetry(){
+    SmartDashboard.putData("Reef", reef);
+  }
+
+  public void periodic(){
+    telemetry();
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
---
+++ src/main/java/frc/robot/commands/ElevatorCommands.java
@@ -0,0 +1,58 @@
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.constants.ElevatorConstants;
+import frc.robot.subsystems.Elevator;
+
+public class ElevatorCommands {
+    Elevator elevator;
+    public ElevatorCommands(Elevator elevator){
+        this.elevator = elevator;
+    }
+
+    public Command setSpeed(DoubleSupplier speed){
+        return new InstantCommand(()->elevator.setSpeed(speed.getAsDouble()),elevator);
+    }
+
+    public Command setGoal(DoubleSupplier goal){
+        return new InstantCommand(()->elevator.setGoal(goal.getAsDouble()),elevator);
+    }
+
+    public Command moveToL1(){
+        System.out.println("x");
+        return new InstantCommand(()->elevator.setGoal(ElevatorConstants.L1goal),elevator);
+    }
+    
+    public Command moveToL2(){
+        return new InstantCommand(()->elevator.setGoal(ElevatorConstants.L2goal),elevator);
+    }
+
+    public Command moveToL3(){
+        return new InstantCommand(()->elevator.setGoal(ElevatorConstants.L3goal),elevator);
+    }
+
+    public Command moveToL4(){
+        return new InstantCommand(()->elevator.setGoal(ElevatorConstants.L4goal),elevator);
+    }
+
+    public Command moveToIntake(){
+        return new InstantCommand(()->elevator.setGoal(ElevatorConstants.intakeGoal),elevator);
+    }
+
+    public Command moveToGroundIntake(){
+        return new InstantCommand(()->elevator.setGoal(ElevatorConstants.groundIntakeGoal),elevator);
+    }
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction dir){
+        return elevator.sysIdRoutine.quasistatic(dir);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction dir){
+        return elevator.sysIdRoutine.dynamic(dir);
+    }
+}
