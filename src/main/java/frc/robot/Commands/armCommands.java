0759f026d905da21f053388bec0d76865487c930
kaileeNguyen <sierrakailee14@gmail.com>
2025-01-18 15:11:55 -0800

    fixed folder names

---
+++ src/main/java/frc/robot/Commands/armCommands.java
@@ -0,0 +1,17 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.armConstants;
+import frc.robot.Subsystems.Arm;
+
+
+public class armCommands{
+     Arm arm;
+        public armCommands(Arm arm) {
+            this.arm = arm;
+        }
+   
+
+}
---
+++ src/main/java/frc/robot/Constants/armConstants.java
@@ -0,0 +1,24 @@
+package frc.robot.Constants;
+
+public class armConstants{
+
+    public static final double armSpeed = 3.0;
+    public static final int motorId  = 1;
+    public static double kP = 0; // enter
+    public static double kI = 0; // enter
+    public static double kD = 0; // enter
+    public static double ks = 0;
+    public static double kg = 0;
+    public static double kv = 0;
+    public static final double min = 0; // pi /180
+    public static final double max = 100;
+    public static final double tolerance = .1;
+    public static final double goal1 = 0; //enter low goal
+    public static final double goal2 = 0; //enter level 2 goal
+    public static final double goal3 = 0; //enter level 3 goal
+    public static final double goal4 = 0; // enter high goal
+    public static final double armOffset = 0.0;
+    public static final double gearRatio = 1.0;
+    public static final double jkg = 500.0;
+    public static final double armLength = 0.594;
+}
