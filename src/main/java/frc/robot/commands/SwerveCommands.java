df964ddc3be9767672958bf4a08ea9e29d3a7e64
JosephSwearingen <jhsweargn@gmail.com>
2025-01-20 19:34:41 -0800

    Added remaining reef poses and logic for selecting the closest side to robot

--- src/main/java/frc/robot/commands/SwerveCommands.java
+++ src/main/java/frc/robot/commands/SwerveCommands.java
@@ -3,6 +3,8 @@ package frc.robot.commands;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -23,23 +25,41 @@ public class SwerveCommands {
         );
     }
 
+    public int getClosestSide() {
+        Pose2d pose = swerve.getPose();
+        if (swerve.onRedAlliance()) {
+            pose = swerve.invertPose(pose);
+        }
+        int closest = 0;
+        double closest_distance = 100.0;
+        for (int i = 0; i < 6; i++) {
+            Transform2d transform = pose.minus(SwerveConstants.centerReef[i]);
+            double distance = Math.hypot(transform.getX(), transform.getY());
+            if (distance < closest_distance) {
+                closest_distance = distance;
+                closest = i;
+            }
+        }
+        return closest;
+    }
+
     public InstantCommand driveToReefLeft() {
         return new InstantCommand (
-            () -> swerve.driveToPose(SwerveConstants.reefA),
+            () -> swerve.driveToPose(SwerveConstants.leftReef[getClosestSide()]),
             swerve
         );
     }
 
     public InstantCommand driveToReefCenter() {
         return new InstantCommand (
-            () -> swerve.driveToPose(SwerveConstants.reefAB),
+            () -> swerve.driveToPose(SwerveConstants.centerReef[getClosestSide()]),
             swerve
         );
     }
 
     public InstantCommand driveToReefRight() {
         return new InstantCommand (
-            () -> swerve.driveToPose(SwerveConstants.reefB),
+            () -> swerve.driveToPose(SwerveConstants.rightReef[getClosestSide()]),
             swerve
         );
     }
