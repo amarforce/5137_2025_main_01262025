ec8dbe53e72e718930dc2a74bd1c332bfdbacf81
kaileeNguyen <sierrakailee14@gmail.com>
2025-01-20 17:48:55 -0800

    added robot commands - unfinished

--- src/main/java/frc/robot/Commands/armCommands.java
+++ src/main/java/frc/robot/Commands/armCommands.java
@@ -1,5 +1,7 @@
 package frc.robot.Commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,5 +15,26 @@ public class armCommands{
             this.arm = arm;
         }
    
+        public Command setSpeed(DoubleSupplier speed) {
+            return new InstantCommand(()-> arm.setSpeed(speed.getAsDouble()), arm);
+        }
+        public Command setGoal(DoubleSupplier goal) {
+            return new InstantCommand(() -> arm.setGoal(goal.getAsDouble()), arm);
+        }
 
+
+        //set to level commands
+
+        public Command move2L1() {
+            return new InstantCommand(() -> arm.setGoal(armConstants.goal1), arm);
+        }
+        public Command move2L2() {
+            return new InstantCommand(() -> arm.setGoal(armConstants.goal2), arm);
+        }
+        public Command move2L3() {
+            return new InstantCommand(() -> arm.setGoal(armConstants.goal3), arm);
+        }
+        public Command move2L4() {
+            return new InstantCommand(() -> arm.setGoal(armConstants.goal4), arm);
+        }
 }
