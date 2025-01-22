8d2af48055227ecd09873baf4a78e59a90770bb3
JosephSwearingen <jhsweargn@gmail.com>
2025-01-21 17:38:11 -0800

    Cleaned up code

--- src/main/java/frc/robot/Commands/armCommands.java
+++ src/main/java/frc/robot/Commands/armCommands.java
@@ -4,37 +4,38 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Arm;
 import frc.robot.constants.ArmConstants;
 
 
 public class ArmCommands{
-     Arm arm;
-        public ArmCommands(Arm arm) {
-            this.arm = arm;
-        }
-   
-        public Command setSpeed(DoubleSupplier speed) {
-            return new InstantCommand(()-> arm.setSpeed(speed.getAsDouble()), arm);
-        }
-        public Command setGoal(DoubleSupplier goal) {
-            return new InstantCommand(() -> arm.setGoal(goal.getAsDouble()), arm);
-        }
 
+    private Arm arm;
 
-        //set to level commands
+    public ArmCommands(Arm arm) {
+        this.arm = arm;
+    }
 
-        public Command moveToL1() {
-            return new InstantCommand(() -> arm.setGoal(ArmConstants.goal1), arm);
-        }
-        public Command moveToL2() {
-            return new InstantCommand(() -> arm.setGoal(ArmConstants.goal2), arm);
-        }
-        public Command moveToL3() {
-            return new InstantCommand(() -> arm.setGoal(ArmConstants.goal3), arm);
-        }
-        public Command moveToL4() {
-            return new InstantCommand(() -> arm.setGoal(ArmConstants.goal4), arm);
-        }
+    public Command setSpeed(DoubleSupplier speed) {
+        return new InstantCommand(()-> arm.setSpeed(speed.getAsDouble()), arm);
+    }
+    public Command setGoal(DoubleSupplier goal) {
+        return new InstantCommand(() -> arm.setGoal(goal.getAsDouble()), arm);
+    }
+
+    public Command moveToL1() {
+        return new InstantCommand(() -> arm.setGoal(ArmConstants.goal1), arm);
+    }
+
+    public Command moveToL2() {
+        return new InstantCommand(() -> arm.setGoal(ArmConstants.goal2), arm);
+    }
+
+    public Command moveToL3() {
+        return new InstantCommand(() -> arm.setGoal(ArmConstants.goal3), arm);
+    }
+
+    public Command moveToL4() {
+        return new InstantCommand(() -> arm.setGoal(ArmConstants.goal4), arm);
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
@@ -13,28 +12,28 @@ import frc.robot.subsystems.*;
 
 public class RobotContainer {
 
-  private Arm arm;
-  private CommandPS5Controller driver;
-  private CommandPS5Controller operator;
-  private ArmCommands armCommands;
+	private Arm arm;
+	private CommandPS5Controller driver;
+	private CommandPS5Controller operator;
+	private ArmCommands armCommands;
 
-  public RobotContainer() {
-    
-    arm = new Arm();
-    driver = new CommandPS5Controller(0);
-    operator = new CommandPS5Controller(1);
-    armCommands = new ArmCommands(arm);
-    configureBindings();
-  }
+	public RobotContainer() {
+		
+		arm = new Arm();
+		driver = new CommandPS5Controller(0);
+		operator = new CommandPS5Controller(1);
+		armCommands = new ArmCommands(arm);
+		configureBindings();
+	}
 
-  private void configureBindings() {
-    operator.triangle().onTrue(armCommands.moveToL4());
-    operator.square().onTrue(armCommands.moveToL3());
-    operator.circle().onTrue(armCommands.moveToL2());
-    operator.cross().onTrue(armCommands.moveToL1());
-  }
+	private void configureBindings() {
+		operator.triangle().onTrue(armCommands.moveToL4());
+		operator.square().onTrue(armCommands.moveToL3());
+		operator.circle().onTrue(armCommands.moveToL2());
+		operator.cross().onTrue(armCommands.moveToL1());
+	}
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+	public Command getAutonomousCommand() {
+		return Commands.print("No autonomous command configured");
+	}
 }
