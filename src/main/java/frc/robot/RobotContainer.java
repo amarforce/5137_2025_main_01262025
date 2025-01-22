2ce75a0f45f9cf6c333f014630ad89ed91d2c436
kaileeNguyen <sierrakailee14@gmail.com>
2025-01-21 17:33:55 -0800

    Added triangle, circle, square, and cross bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,22 +4,35 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import frc.robot.commands.ArmCommands;
 import frc.robot.subsystems.*;
 
 public class RobotContainer {
 
   private Arm arm;
+  private CommandPS5Controller driver;
+  private CommandPS5Controller operator;
+  private ArmCommands armCommands;
 
   public RobotContainer() {
     
     arm = new Arm();
-
+    driver = new CommandPS5Controller(0);
+    operator = new CommandPS5Controller(1);
+    armCommands = new ArmCommands(arm);
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    operator.triangle().onTrue(armCommands.moveToL4());
+    operator.square().onTrue(armCommands.moveToL3());
+    operator.circle().onTrue(armCommands.moveToL2());
+    operator.cross().onTrue(armCommands.moveToL1());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
